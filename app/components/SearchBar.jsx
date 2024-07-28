652859bedf34cc37f291a5205d179213fcc243af
ledionres <ledionrestelica7@gmail.com>
2024-07-28 14:44:15 +0200

    Fix style to search results

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -60,10 +60,10 @@ export default function SearchBar() {
       />
       {focus && filteredProducts.length > 0 && (
         <div className="relative">
-          <div className="absolute bg-[#E9E9E9] top-0 w-full border border-[#E9E9E9] rounded-[8px] z-10">
+          <div className="absolute bg-white top-0 w-full  rounded-[8px] z-10">
             {filteredProducts.map((product) => (
               <Link href={`/product/${product._id}`} key={product._id}>
-                <div className="py-3 px-4 cursor-pointer hover:bg-white">
+                <div className="py-3 px-4  border border-t-[#E9E9E9] cursor-pointer hover:bg-[#E9E9E9]">
                   <h3>{product.name}</h3>
                 </div>
               </Link>
