199dc6bc9158747cb9ccbb4fd275822b32e5954d
ledionres <ledionrestelica7@gmail.com>
2024-08-21 14:37:58 +0200

    Request account button

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -5,8 +5,11 @@ export default function Page() {
     <div className="w-full min-h-[100vh] gap-4 flex flex-col justify-center items-center">
       <SignIn />
       <div className="flex flex-col justify-between items-center">
-        <div>Request an account</div>
-        <Link href="/request-account">here</Link>
+        <Link href="/request-account">
+          <div className="w-max px-3 flex items-center h-[43px] text-[14px] bg-primary  rounded-[8px] cursor-pointer">
+            Request Account
+          </div>
+        </Link>
       </div>
     </div>
   );
